1c1edb8bd5a99340f7f07e6d3f1d63a9551d82a1
sviatoslavtkachuk <sviatoslav.tkachuk11@gmail.com>
2026-02-16 13:24:37 +0200

    #33: Create prompts

--- services/rag-service/src/pdf_parsing/extract_algorithms_and_examples.py
+++ services/rag-service/src/pdf_parsing/extract_algorithms_and_examples.py
@@ -86,6 +86,7 @@ def _extract_page_algorithms_and_examples_by_caption(captions, page):
 
                     gray_block = caption
                     gray_block["text"] = page.get_text("text", clip=drawing_rect)
+                    gray_block["bbox"] = tuple(drawing_rect)
                     gray_block["caption_text"] = page.get_text("text", clip=caption["caption_rect"])
 
                     # replace caption_rect with caption_bbox for consistency
@@ -118,7 +119,8 @@ def _extract_page_algorithms_and_examples(page):
                 gray_block = {
                     "caption": caption_text,
                     "caption_rect": caption_rect,
-                    "block_type": block_type
+                    "block_type": block_type,
+                    "page_number": page.number,
                 }
                 # Keep track of candidate captions so the next stage can find gray fills nearby.
                 page_gray_block_captions.append(gray_block)
@@ -137,6 +139,8 @@ def extract_algorithms_and_examples(file_path):  # extract algorithms and exampl
     return algorithms_and_examples
 
 def get_algorithm_caption_from_jsons(algorithm_number):
+    max_x = 0
+    max_y = 0
     count = 0
     for json_path in sorted(Path("pdf_jsons").glob("*.json")):
         with json_path.open("r", encoding="utf-8") as fh:
@@ -151,7 +155,12 @@ def get_algorithm_caption_from_jsons(algorithm_number):
                     idx = clean.find(algorithm_number)
                     result = clean[idx:]
                     return result
+                # if element["block_bbox"][2] > max_x:
+                #     max_x = element["block_bbox"][2]
+                # if element["block_bbox"][3] > max_y:
+                #     max_y = element["block_bbox"][3]
     print(f"Found {count} captions containing '{algorithm_number}' in JSON files.")
+    print("RR: ", max_x, max_y)
 
 
 def extract_algorithms(pymu_clocks):
@@ -170,5 +179,95 @@ def extract_algorithms(pymu_clocks):
         print(algorith)
     return algorithms
 
-res = extract_algorithms(extract_algorithms_and_examples("dm.pdf"))
-print(res)
+def get_example_by_number(example_number, pymu_clocks):
+    for block in pymu_clocks:
+        if block["block_type"] != BlockType.EXAMPLE.value:
+            continue
+
+        if example_number in block["caption"]:
+            return block
+    return None
+
+
+
+def is_inside(big, small):
+    x1b, y1b, x2b, y2b = big[0] / 576, big[1] / 648, big[2] / 576, big[3] / 648
+    x1s, y1s, x2s, y2s = small[0] / 1094, small[1] / 1235, small[2] / 1094, small[3] / 1235
+
+    return (
+        x1b <= x1s and
+        y1b <= y2s and
+        x2b + 0.05 >= x2s and
+        y2b >= y1s
+    )
+
+def get_example_caption(json_page, example_number):
+    for element in json_page['prunedResult']['parsing_res_list']:
+        if example_number in element["block_content"]:
+            # return element["block_content"]
+            text = element["block_content"]
+            clean = re.sub(r'<[^>]+>', '', text)
+            idx = clean.find(example_number)
+            result = clean[idx:]
+            return result
+
+
+def extract_example_from_jsons(example):
+    example_number = f"{example["caption"].split(" ")[0]} {example["caption"].split(" ")[1]}"
+    example_bbox = example["bbox"]
+    json_number = example["page_number"] // 100
+    json_path = sorted(Path("pdf_jsons").glob("*.json"))[json_number]
+
+    with json_path.open("r", encoding="utf-8") as fh:
+        json_data = json.load(fh)
+
+    result_text = ""
+    page = json_data[example["page_number"] % 100]
+    for element in page['prunedResult']['parsing_res_list']:
+        if is_inside(example_bbox, element["block_bbox"]):
+            # return element["block_content"]
+            text = element["block_content"]
+            clean = re.sub(r'<[^>]+>', '', text)
+            result_text += f"{clean}\n"
+
+    idx = result_text.find(example_number)
+    if idx != -1:
+        return {
+            "caption": result_text[idx:],
+            "text": result_text[:idx]
+        }
+    else:
+        return {
+            "caption": get_example_caption(page, example_number),
+            "text": result_text[:idx]
+        }
+    return result_text#[idx:]
+
+# res = extract_algorithms(extract_algorithms_and_examples("dm.pdf"))
+# print(res)
+pymu_clocks = extract_algorithms_and_examples("dm.pdf")
+# example = get_example_by_number("Example 17.2.", pymu_clocks)
+# example = get_example_by_number("Example 17.2.", pymu_clocks)
+# print(extract_example_from_jsons(example))
+
+example_with_code_numbers = [
+    "Example 15.2.",
+    "Example 17.2.",
+    "Example 17.3.",
+    "Example 17.4.",
+    "Example 21.1.",
+    "Example 22.1.",
+    "Example 22.3.",
+    "Example 22.6.",
+    "Example 2.3.",
+    "Example 2.5.",
+    "Example 4.1.",
+    "Example 4.2.",
+    "Example 9.10.",
+    "Example 10.1.",
+    "Example 11.2.",
+]
+
+for example_number in example_with_code_numbers:
+    example = get_example_by_number(example_number, pymu_clocks)
+    print(extract_example_from_jsons(example))
