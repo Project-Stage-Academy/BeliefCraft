8a1b746c1e6f03264362a58bf9054b5c74c79522
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-03-03 13:41:08 +0200

    feat(api): add automatic generating of openapi specs

---
+++ scripts/ci/generate_openapi.py
@@ -0,0 +1,19 @@
+import yaml
+from pathlib import Path
+
+from environment_api.main import app
+
+def generate_openapi():
+    schema = app.openapi()
+
+    out_dir = Path(__file__).parent.parent.parent / "docs" / "api"
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    out_file = out_dir / "environment-openapi.yaml"
+
+    with open(out_file, "w", encoding="utf-8") as f:
+        yaml.dump(schema, f, sort_keys=False, allow_unicode=True)
+
+
+if __name__ == "__main__":
+    generate_openapi()
