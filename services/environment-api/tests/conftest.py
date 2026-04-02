495511871e8525fdfe5a75ba1afff150e50d96ac
Anton Mykhalchuk <anton.mykhalchuk@icloud.com>
2026-04-02 18:35:54 +0300

    fix(env): add imports to conftest

---
+++ services/environment-api/tests/conftest.py
@@ -0,0 +1,13 @@
+from common.shared_fixtures import (
+    client,
+    db_engine,
+    db_session,
+    seed_base_world,
+)
+
+__all__ = [
+    "client",
+    "db_engine",
+    "db_session",
+    "seed_base_world",
+]
